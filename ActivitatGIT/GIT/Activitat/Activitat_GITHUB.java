1dbaa45b4d8abe80d95bdf65b4c4eb7b070b7961
Your Name <you@example.com>
2024-11-19 18:49:20 +0100

    resta feta

--- ActivitatGIT/GIT/Activitat/Activitat_GITHUB.java
+++ ActivitatGIT/GIT/Activitat/Activitat_GITHUB.java
@@ -10,15 +10,6 @@ public class Activitat_GITHUB {
 				int[] subArray = {785675, 56, 5675, 56544, 234};
 				int[] prodArray = {6, 8, 20, 23, 12};
 				
-				// User 1 fer la suma dels elements de sumArray
-				
-				int suma = 0; 
-				
-				for(int i = 0; i < sumArray.length; i++) {
-					suma += sumArray[i];
-				}		
-					System.out.println("El resultat es: " + suma);
-
 	
 				// User 2 fer la resta dels elements de subArray
 						
