8dd2a0b2ec5e2ae9310779b71e861e3176be62ac
Aarongp13 <aarongarcia132005@gmail.com>
2024-12-02 22:52:42 +0100

    Afeguir el arxiu de comandes

---
+++ Debugging/Repositorio-GitHub/ActivitatGIT/GIT/Activitat/Activitat_GITHUB.java
@@ -0,0 +1,31 @@
+package Activitat;
+
+public class Activitat_GITHUB {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+				
+				int[] sumArray = {4,36,4,23,867};
+				int[] subArray = {785675, 56, 5675, 56544, 234};
+				int[] prodArray = {6, 8, 20, 23, 12};
+				
+	
+				// User 2 fer la resta dels elements de subArray
+						
+					int resta = 0;
+				
+					for(int f = 0; f < subArray.length; f++) {
+						resta -= subArray[f];
+					
+					}
+					
+					System.out.println("El resultat de la resta es: " + resta);
+					
+			
+	
+
+	}
+	
+
+}				
